8250016f04cf3dd61741c167e13156e0ab24acb1
맹주영 <mjy9088@naver.com>
2019-03-21 23:35:41 +0900

    list.h

---
+++ list.h
@@ -0,0 +1,13 @@
+#ifndef __mtl_list
+#define __mtl_list
+
+typedef struct _tagMTL_List
+{
+	void *type;
+} *MTL_List;
+
+void MTL_List_length(MTL_List *self);
+void MTL_List_set(MTL_List *self, int idx, void *value);
+void MTL_List_set(MTL_List *self, int idx, void **value);
+
+#endif
