11e898894ed58259e6a60cda329c9a14e594339e
맹주영 <mjy9088@naver.com>
2019-03-21 23:41:33 +0900

    rename wrong named function

--- list.h
+++ list.h
@@ -8,6 +8,6 @@ typedef struct _tagMTL_List
 
 void MTL_List_length(MTL_List self);
 void MTL_List_set(MTL_List self, int idx, void *value);
-void MTL_List_set(MTL_List self, int idx, void **value);
+void MTL_List_get(MTL_List self, int idx, void **value);
 
 #endif
