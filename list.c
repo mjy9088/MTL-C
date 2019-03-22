e81abac6af58cf064a68d8ba2de2879b1128f9d7
맹주영 <mjy9088@naver.com>
2019-03-23 01:51:54 +0900

    Changed list definition

--- list.c
+++ list.c
@@ -3,8 +3,8 @@
 typedef struct
 {
 	int (*length)(MTL_List self);
-	void (*set)(MTL_List self, int idx, void *value);
-	void *(*get)(MTL_List self, int idx);
+	bool (*set)(MTL_List self, int idx, void *value);
+	bool (*get)(MTL_List self, int idx);
 } *MTLDEF_List;
 
 int MTL_List_length(MTL_List self)
@@ -12,13 +12,13 @@ int MTL_List_length(MTL_List self)
 	return ((MTLDEF_List)self->type)->length(self);
 }
 
-void MTL_List_set(MTL_List self, int idx, void *value)
+bool MTL_List_set(MTL_List self, int idx, void *value)
 {
-	((MTLDEF_List)self->type)->set(self, idx, value);
+	return ((MTLDEF_List)self->type)->set(self, idx, value);
 }
 
-void *MTL_List_get(MTL_List self, int idx)
+bool MTL_List_get(MTL_List self, int idx, void **out)
 {
-	return ((MTLDEF_List)self->type)->get(self, idx);
+	return ((MTLDEF_List)self->type)->get(self, idx, out);
 }
 
--- list.h
+++ list.h
@@ -7,7 +7,7 @@ typedef struct _tagMTL_List
 } *MTL_List;
 
 int MTL_List_length(MTL_List self);
-void MTL_List_set(MTL_List self, int idx, void *value);
-void *MTL_List_get(MTL_List self, int idx);
+bool MTL_List_set(MTL_List self, int idx, void *value);
+bool MTL_List_get(MTL_List self, int idx, void **value);
 
 #endif
