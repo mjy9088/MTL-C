40a0b4a729b11172db74b94cc1facc70cab37e4b
맹주영 <mjy9088@naver.com>
2019-03-22 23:54:34 +0900

    Add declaration

--- list/arraylist.h
+++ list/arraylist.h
@@ -12,4 +12,8 @@ typedef struct _tagMTL_ArrayList
 
 MTL_ArrayList new_MTL_ArrayList(int length);
 
+int MTL_ArrayList_length(MTL_ArrayList self);
+void MTL_ArrayList_set(MTL_ArrayList self, int idx, void *value);
+void *MTL_ArrayList_get(MTL_ArrayList self, int idx);
+
 #endif
