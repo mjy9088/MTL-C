25b7763c22a0af778bb391bc65077ee54371acc5
맹주영 <mjy9088@naver.com>
2019-03-22 23:57:54 +0900

    Added linkedlist.c

---
+++ list/linkedlist.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include "linkedlist.h"
+
+struct
+{
+	int (*length)(MTL_LinkedList self);
+	void (*set)(MTL_LinkedList self, int idx, void *value);
+	void *(*get)(MTL_LinkedList self, int idx);
+} MTLDEF_LinkedList;
+
+MTL_ArrayList new_MTL_LinkedList()
+{
+	MTL_LinkedList ret = malloc(sizeof(struct _tagMTL_LinkedList));
+	if(!ret) return NULL;
+	ret->type = &MTLDEF_LinkedList;
+	ret->length = 0;
+	return ret;
+}
+
+__attribute__((constructor)) static void init()
+{
+	MTLDEF_ArrayList.length = &MTL_ArrayList_length;
+	MTLDEF_ArrayList.set = &MTL_ArrayList_set;
+	MTLDEF_ArrayList.get = &MTL_ArrayList_get;
+}
+
