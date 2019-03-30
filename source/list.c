d064393658b0b8fa860c41d15cf48a8be8bc08f2
맹주영 <mjy9088@naver.com>
2019-03-30 20:44:33 +0900

    code style

--- source/list.c
+++ source/list.c
@@ -10,21 +10,21 @@ typedef struct
 
 int MTL_List_length(MTL_List self)
 {
-	return ((MTLDEF_List)self->type)->length(self);
+	return (*((MTLDEF_List)self->type)->length)(self);
 }
 
 bool MTL_List_set(MTL_List self, int idx, void *value)
 {
-	return ((MTLDEF_List)self->type)->set(self, idx, value);
+	return (*((MTLDEF_List)self->type)->set)(self, idx, value);
 }
 
 bool MTL_List_get(MTL_List self, int idx, void **out)
 {
-	return ((MTLDEF_List)self->type)->get(self, idx, out);
+	return (*((MTLDEF_List)self->type)->get)(self, idx, out);
 }
 
 void MTL_List_release(MTL_List self)
 {
-	((MTLDEF_List)self->type)->release(self);
+	(*((MTLDEF_List)self->type)->release)(self);
 }
 
