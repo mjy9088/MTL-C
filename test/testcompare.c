0dc4280a9ad2f596246dc927caee562f9eff2f5a
맹주영 <mjy9088@naver.com>
2019-06-18 23:46:33 +0900

    Update testcompare.c

--- test/testcompare.c
+++ test/testcompare.c
@@ -2,5 +2,51 @@
 
 int main(int argc, char **argv)
 {
+	if(argc != 3)
+	{
+		printf("usage: %s file1 file2\n", argv[0]);
+		return -1;
+	}
+	FILE *f1 = fopen(argv[1], "rb");
+	if(!f1)
+	{
+		printf("Failed to open file %s\n", argv[1]);
+		return -2;
+	}
+	FILE *f2 = fopen(argv[1], "rb");
+	if(!f2)
+	{
+		fclose(f1);
+		printf("Failed to open file %s\n", argv[2]);
+		return -3;
+	}
+	char buf1[1024], buf2[1024];
+	int read1, read2, i;
+	while(!feof(f1) && !feof(f2))
+	{
+		read1 = fread(buf1, sizeof(char), 1024, f1);
+		read2 = fread(buf2, sizeof(char), 1024, f2);
+		if(read1 != read2)
+		{
+			fclose(f1);
+			fclose(f2);
+			return 2;
+		}
+		for(i = 0; i < read1; i++)
+		{
+			if(buf1[i] != buf2[i])
+			{
+				fclose(f1);
+				fclose(f2);
+				return 1;
+			}
+		}
+	}
+	if(!feof(f1) != !feof(f2))
+	{
+		fclose(f1);
+		fclose(f2);
+		return 2;
+	}
     return 0;
 }
